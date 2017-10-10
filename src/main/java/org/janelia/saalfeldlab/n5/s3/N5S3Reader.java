9e317076d589ccb34a7010ae9555e971897b85b8
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-10-10 15:09:32 -0400

    first cut at s3-based implementation

---
+++ src/main/java/org/janelia/saalfeldlab/n5/s3/N5S3Reader.java
@@ -0,0 +1,269 @@
+/**
+ * Copyright (c) 2017, Stephan Saalfeld
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.janelia.saalfeldlab.n5.s3;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.nio.channels.Channels;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.janelia.saalfeldlab.n5.BlockReader;
+import org.janelia.saalfeldlab.n5.CompressionType;
+import org.janelia.saalfeldlab.n5.DataBlock;
+import org.janelia.saalfeldlab.n5.DataType;
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.N5Reader;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * Amazon Web Services S3 implementation of the {@link N5Reader} interface.
+ *
+ * @author Igor Pisarev
+ */
+public class N5S3Reader implements N5Reader {
+
+	protected static final String jsonFile = "attributes.json";
+
+	protected final AmazonS3 s3;
+	protected final String bucket;
+	protected final Gson gson;
+
+	/**
+	 * Opens an {@link N5Reader} at a given base path with a custom
+	 * {@link GsonBuilder} to support custom attributes.
+	 *
+	 * If the base path does not exist, it will not be created and all
+	 * subsequent attempts to read attributes, groups, or datasets
+	 * will fail with an {@link IOException}.
+	 *
+	 * @param basePath n5 base path
+	 * @param gsonBuilder
+	 */
+	public N5S3Reader(final String bucket, final GsonBuilder gsonBuilder) {
+
+		this.bucket = bucket;
+		this.s3 = AmazonS3ClientBuilder.defaultClient();
+
+		gsonBuilder.registerTypeAdapter(DataType.class, new DataType.JsonAdapter());
+		gsonBuilder.registerTypeAdapter(CompressionType.class, new CompressionType.JsonAdapter());
+		this.gson = gsonBuilder.create();
+	}
+
+	/**
+	 * Opens an {@link N5Reader} at a given base path.
+	 *
+	 * If the base path does not exist, it will not be created and all
+	 * subsequent attempts to read or write attributes, groups, or datasets
+	 * will fail with an {@link IOException}.
+	 *
+	 * @param basePath n5 base path
+	 */
+	public N5S3Reader(final String bucket) {
+
+		this(bucket, new GsonBuilder());
+	}
+
+	/**
+	 * Reads or creates the attributes map of a group or dataset.
+	 *
+	 * @param pathName group path
+	 * @return
+	 * @throws IOException
+	 *
+	 * TODO uses file locks to synchronize with other processes, now also
+	 *   synchronize for threads inside the JVM
+	 */
+	@Override
+	public HashMap<String, JsonElement> getAttributes(final String pathName) throws IOException {
+
+		final String metadataKey = Paths.get(pathName, jsonFile).toString();
+		if (!s3.doesObjectExist(bucket, metadataKey))
+			return new HashMap<>();
+
+		try (final InputStream in = s3.getObject(bucket, metadataKey).getObjectContent()) {
+			final Type mapType = new TypeToken<HashMap<String, JsonElement>>(){}.getType();
+			final HashMap<String, JsonElement> map = gson.fromJson(new InputStreamReader(in, "UTF-8"), mapType);
+			return map == null ? new HashMap<>() : map;
+		}
+	}
+
+	@Override
+	public <T> T getAttribute(final String pathName, final String key, final Class<T> clazz) throws IOException {
+		final HashMap<String, JsonElement> map = getAttributes(pathName);
+		final JsonElement attribute = map.get(key);
+		if (attribute != null)
+			return gson.fromJson(attribute, clazz);
+		else
+			return null;
+	}
+
+	@Override
+	public DatasetAttributes getDatasetAttributes(final String pathName) throws IOException {
+
+		final HashMap<String, JsonElement> attributes = getAttributes(pathName);
+
+		final JsonElement dimensionsElement = attributes.get(DatasetAttributes.dimensionsKey);
+		if (dimensionsElement == null)
+			return null;
+		final long[] dimensions = gson.fromJson(dimensionsElement, long[].class);
+		if (dimensions == null)
+			return null;
+
+		final JsonElement dataTypeElement = attributes.get(DatasetAttributes.dataTypeKey);
+		if (dataTypeElement == null)
+			return null;
+		final DataType dataType = gson.fromJson(dataTypeElement, DataType.class);
+		if (dataType == null)
+			return null;
+
+		final JsonElement blockSizeElement = attributes.get(DatasetAttributes.blockSizeKey);
+		int[] blockSize = null;
+		if (blockSizeElement != null)
+			blockSize = gson.fromJson(blockSizeElement, int[].class);
+		if (blockSize == null)
+			blockSize = Arrays.stream(dimensions).mapToInt(a -> (int)a).toArray();
+
+		final JsonElement compressionTypeElement = attributes.get(DatasetAttributes.compressionTypeKey);
+		CompressionType compressionType = null;
+		if (compressionTypeElement == null)
+			return null;
+		compressionType = gson.fromJson(compressionTypeElement, CompressionType.class);
+		if (compressionType == null)
+			compressionType = CompressionType.RAW;
+
+		return new DatasetAttributes(dimensions, blockSize, dataType, compressionType);
+	}
+
+	@Override
+	public DataBlock<?> readBlock(
+			final String pathName,
+			final DatasetAttributes datasetAttributes,
+			final long[] gridPosition) throws IOException {
+
+		final String dataBlockRelativePath = getDataBlockPath(pathName, gridPosition).toString();
+		final String dataBlockKey = Paths.get(pathName, dataBlockRelativePath).toString();
+		if (!s3.doesObjectExist(bucket, dataBlockKey))
+			return null;
+
+		try (final InputStream in = s3.getObject(bucket, dataBlockKey).getObjectContent()) {
+			final DataInputStream dis = new DataInputStream(in);
+			final short mode = dis.readShort();
+			final int nDim = dis.readShort();
+			final int[] blockSize = new int[nDim];
+			for (int d = 0; d < nDim; ++d)
+				blockSize[d] = dis.readInt();
+			final int numElements;
+			switch (mode) {
+			case 1:
+				numElements = dis.readInt();
+				break;
+			default:
+				numElements = DataBlock.getNumElements(blockSize);
+			}
+			final DataBlock<?> dataBlock = datasetAttributes.getDataType().createDataBlock(blockSize, gridPosition, numElements);
+
+			final BlockReader reader = datasetAttributes.getCompressionType().getReader();
+			reader.read(dataBlock, Channels.newChannel(in));
+			return dataBlock;
+		}
+	}
+
+	@Override
+	public boolean exists(final String pathName) {
+
+		return
+				!s3.listObjects(bucket, pathName).getObjectSummaries().isEmpty() // contains objects with pathName prefix
+				&&
+				!s3.doesObjectExist(bucket, pathName) // pathName itself is not an actual object
+			;
+	}
+
+	@Override
+	public boolean datasetExists(final String pathName) throws IOException {
+
+		return exists(pathName) && getDatasetAttributes(pathName) != null;
+	}
+
+	/**
+	 * Creates the path for a data block in a dataset at a given grid position.
+	 *
+	 * The returned path is
+	 * <pre>
+	 * $datasetPathName/$gridPosition[0]/$gridPosition[1]/.../$gridPosition[n]
+	 * </pre>
+	 *
+	 * This is the file into which the data block will be stored.
+	 *
+	 * @param datasetPathName
+	 * @param gridPosition
+	 * @return
+	 */
+	public static Path getDataBlockPath(final String datasetPathName, final long[] gridPosition) {
+
+		final String[] pathComponents = new String[gridPosition.length];
+		for (int i = 0; i < pathComponents.length; ++i)
+			pathComponents[i] = Long.toString(gridPosition[i]);
+
+		return Paths.get(
+				datasetPathName,
+				pathComponents);
+	}
+
+	@Override
+	public String[] list(final String pathName) throws IOException {
+
+		final Path path = Paths.get(pathName);
+		final List<S3ObjectSummary> subObjects = s3.listObjects(bucket, pathName).getObjectSummaries();
+		final Set<String> subGroups = new HashSet<>();
+		for (final S3ObjectSummary subObject : subObjects)
+		{
+			final Path keyPath = Paths.get(subObject.getKey());
+			final Path relativeKeyPath = path.relativize(keyPath);
+			if (relativeKeyPath.getNameCount() > 1) // not an actual object
+			{
+				final Path subgroupPath = relativeKeyPath.subpath(0, 1);
+				subGroups.add(subgroupPath.toString());
+			}
+		}
+		return subGroups.toArray(new String[subGroups.size()]);
+	}
+}
---
+++ src/main/java/org/janelia/saalfeldlab/n5/s3/N5S3Writer.java
@@ -0,0 +1,233 @@
+/**
+ * Copyright (c) 2017, Stephan Saalfeld
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.janelia.saalfeldlab.n5.s3;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.nio.channels.Channels;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.janelia.saalfeldlab.n5.BlockWriter;
+import org.janelia.saalfeldlab.n5.CompressionType;
+import org.janelia.saalfeldlab.n5.DataBlock;
+import org.janelia.saalfeldlab.n5.DataType;
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.N5Writer;
+
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * A simple structured container format for hierarchies of chunked
+ * n-dimensional datasets and attributes.
+ *
+ * {@linkplain https://github.com/axtimwalde/n5}
+ *
+ * @author Stephan Saalfeld
+ */
+public class N5S3Writer extends N5S3Reader implements N5Writer {
+
+	/**
+	 * Opens an {@link N5Writer} at a given base path with a custom
+	 * {@link GsonBuilder} to support custom attributes.
+	 *
+	 * If the base path does not exist, it will not be created and all
+	 * subsequent attempts to read or write attributes, groups, or datasets
+	 * will fail with an {@link IOException}.
+	 *
+	 * If the base path is not writable, all subsequent attempts to write
+	 * attributes, groups, or datasets will fail with an {@link IOException}.
+	 *
+	 * @param basePath n5 base path
+	 * @param gsonBuilder
+	 */
+	public N5S3Writer(final String bucket, final GsonBuilder gsonBuilder) {
+
+		super(bucket, gsonBuilder);
+	}
+
+	/**
+	 * Opens an {@link N5Writer} at a given base path.
+	 *
+	 * If the base path does not exist, it will not be created and all
+	 * subsequent attempts to read or write attributes, groups, or datasets
+	 * will fail with an {@link IOException}.
+	 *
+	 * If the base path is not writable, all subsequent attempts to write
+	 * attributes, groups, or datasets will fail with an {@link IOException}.
+	 *
+	 * @param basePath n5 base path
+	 */
+	public N5S3Writer(final String bucket) {
+
+		this(bucket, new GsonBuilder());
+	}
+
+	@Override
+	public <T> void setAttribute(final String pathName, final String key, final T attribute) throws IOException {
+
+		setAttributes(pathName, Collections.singletonMap(key, attribute));
+	}
+
+	@Override
+	public void setAttributes(final String pathName, final Map<String, ?> attributes) throws IOException {
+
+		final String metadataKey = Paths.get(pathName, jsonFile).toString();
+
+		final HashMap<String, JsonElement> map = getAttributes(pathName);
+		for (final Entry<String, ?> entry : attributes.entrySet())
+			map.put(entry.getKey(), gson.toJsonTree(entry.getValue()));
+
+		final Type mapType = new TypeToken<HashMap<String, JsonElement>>(){}.getType();
+ 		final String json = gson.toJson(map, mapType);
+
+		try (final InputStream data = new ByteArrayInputStream(json.getBytes("UTF-8"))) {
+			s3.putObject(bucket, metadataKey, data, new ObjectMetadata());
+		}
+	}
+
+	@Override
+	public void createGroup(final String pathName) throws IOException {
+
+		final Path path = Paths.get(pathName);
+		for (int i = 0; i < path.getNameCount(); ++i)
+			setAttributes(path.subpath(0, i + 1).toString(), Collections.emptyMap());
+	}
+
+	/**
+	 * Removes a group or dataset (directory and all contained files).
+	 *
+	 * <p><code>{@link #remove(String) remove("")}</code> or
+	 * <code>{@link #remove(String) remove("")}</code> will delete this N5
+	 * container.  Please note that no checks for safety will be performed,
+	 * e.g. <code>{@link #remove(String) remove("..")}</code> will try to
+	 * recursively delete the parent directory of this N5 container which
+	 * only fails because it attempts to delete the parent directory before it
+	 * is empty.
+	 *
+	 * @param pathName group path
+	 * @throws IOException
+	 */
+	@Override
+	public boolean remove(final String pathName) throws IOException {
+
+		final List<String> objectsToDelete = new ArrayList<>();
+
+		final List<S3ObjectSummary> subObjects = s3.listObjects(bucket, pathName).getObjectSummaries();
+		for (final S3ObjectSummary subObject : subObjects)
+			objectsToDelete.add(subObject.getKey());
+
+		s3.deleteObjects(new DeleteObjectsRequest(bucket).withKeys(objectsToDelete.toArray(new String[objectsToDelete.size()])));
+		return true;
+	}
+
+	/**
+	 * Creates a dataset.  This does not create any data but the path and
+	 * mandatory attributes only.
+	 *
+	 * @param pathName dataset path
+	 * @param datasetAttributes
+	 * @throws IOException
+	 */
+	@Override
+	public void createDataset(
+			final String pathName,
+			final DatasetAttributes datasetAttributes) throws IOException{
+
+		createGroup(pathName);
+		setDatasetAttributes(pathName, datasetAttributes);
+	}
+
+	/**
+	 * Creates a dataset.  This does not create any data but the path and
+	 * mandatory attributes only.
+	 *
+	 * @param pathName dataset path
+	 * @param dimensions
+	 * @param blockSize
+	 * @param dataType
+	 * @throws IOException
+	 */
+	@Override
+	public void createDataset(
+			final String pathName,
+			final long[] dimensions,
+			final int[] blockSize,
+			final DataType dataType,
+			final CompressionType compressionType) throws IOException {
+
+		createGroup(pathName);
+		setDatasetAttributes(pathName, new DatasetAttributes(dimensions, blockSize, dataType, compressionType));
+	}
+
+	@Override
+	public <T> void writeBlock(
+			final String pathName,
+			final DatasetAttributes datasetAttributes,
+			final DataBlock<T> dataBlock) throws IOException {
+
+		final String dataBlockRelativePath = getDataBlockPath(pathName, dataBlock.getGridPosition()).toString();
+		final String dataBlockKey = Paths.get(pathName, dataBlockRelativePath).toString();
+
+		try (final ByteArrayOutputStream byteStream = new ByteArrayOutputStream()) {
+			final DataOutputStream dos = new DataOutputStream(byteStream);
+
+			final int mode = (dataBlock.getNumElements() == DataBlock.getNumElements(dataBlock.getSize())) ? 0 : 1;
+			dos.writeShort(mode);
+
+			dos.writeShort(datasetAttributes.getNumDimensions());
+			for (final int size : dataBlock.getSize())
+				dos.writeInt(size);
+
+			if (mode != 0)
+				dos.writeInt(dataBlock.getNumElements());
+
+			dos.flush();
+
+			final BlockWriter writer = datasetAttributes.getCompressionType().getWriter();
+			writer.write(dataBlock, Channels.newChannel(dos));
+
+			try (final InputStream data = new ByteArrayInputStream(byteStream.toByteArray())) {
+				s3.putObject(bucket, dataBlockKey, data, new ObjectMetadata());
+			}
+		}
+	}
+}
