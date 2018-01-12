85a6bb6f673b87443e894b2c56bc03dd8c016619
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-01-12 15:59:05 -0500

    javadoc improvements

--- src/test/java/org/janelia/saalfeldlab/n5/AbstractN5VersionedTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/AbstractN5VersionedTest.java
@@ -1,3 +1,19 @@
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package org.janelia.saalfeldlab.n5;
 
 import java.io.IOException;
@@ -5,6 +21,13 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * Abstract base class for testing versioned N5 functionality.
+ * Subclasses are expected to provide a specific N5 implementation to be tested by defining the {@link #createN5Writer()} method.
+ *
+ * @author Stephan Saalfeld &lt;saalfelds@janelia.hhmi.org&gt;
+ * @author Igor Pisarev &lt;pisarevi@janelia.hhmi.org&gt;
+ */
 public abstract class AbstractN5VersionedTest extends AbstractN5Test {
 
 	@Override
