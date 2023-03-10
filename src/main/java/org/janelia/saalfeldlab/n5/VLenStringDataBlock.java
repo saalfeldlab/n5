d234d37de0b55bf45a3a1772ecbe852836a52584
Michael Innerberger <innerbergerm@hhmi.org>
2023-03-10 14:26:23 -0500

    Defer (de-)serialization until data is needed

--- src/main/java/org/janelia/saalfeldlab/n5/VLenStringDataBlock.java
+++ src/main/java/org/janelia/saalfeldlab/n5/VLenStringDataBlock.java
@@ -34,23 +34,23 @@ public class VLenStringDataBlock extends AbstractDataBlock<String[]> {
 
     protected static final Charset ENCODING = StandardCharsets.UTF_8;
     protected static final String NULLCHAR = "\0";
-    protected byte[] serializedData;
-    protected String[] actualData;
+    protected byte[] serializedData = null;
+    protected String[] actualData = null;
 
     public VLenStringDataBlock(final int[] size, final long[] gridPosition, final String[] data) {
         super(size, gridPosition, new String[0]);
         actualData = data;
-        serializedData = serialize(data);
     }
 
     public VLenStringDataBlock(final int[] size, final long[] gridPosition, final byte[] data) {
         super(size, gridPosition, new String[0]);
         serializedData = data;
-        actualData = deserialize(data);
     }
 
     @Override
     public ByteBuffer toByteBuffer() {
+        if (serializedData == null)
+            serializedData = serialize(actualData);
         return ByteBuffer.wrap(serializedData);
     }
 
@@ -61,23 +61,27 @@ public class VLenStringDataBlock extends AbstractDataBlock<String[]> {
         actualData = deserialize(buffer.array());
     }
 
-    static protected byte[] serialize(String[] strings) {
+    protected byte[] serialize(String[] strings) {
         final String flattenedArray = String.join(NULLCHAR, strings) + NULLCHAR;
         return flattenedArray.getBytes(ENCODING);
     }
 
-    static protected String[] deserialize(byte[] rawBytes) {
+    protected String[] deserialize(byte[] rawBytes) {
         final String rawChars = new String(rawBytes, ENCODING);
         return rawChars.split(NULLCHAR);
     }
 
     @Override
     public int getNumElements() {
+        if (serializedData == null)
+            serializedData = serialize(actualData);
         return serializedData.length;
     }
 
     @Override
     public String[] getData() {
+        if (actualData == null)
+            actualData = deserialize(serializedData);
         return actualData;
     }
 }
