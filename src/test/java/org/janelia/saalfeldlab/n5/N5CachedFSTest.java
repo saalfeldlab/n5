0715550f0b5384ae869b35ee4a2434abbcfcf085
John Bogovic <bogovicj@janelia.hhmi.org>
2023-04-13 10:39:51 -0400

    test(style): caching of groups and datasets

--- src/test/java/org/janelia/saalfeldlab/n5/N5CachedFSTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/N5CachedFSTest.java
@@ -4,6 +4,11 @@ import com.google.gson.GsonBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,12 +17,20 @@ import java.util.ArrayList;
 
 public class N5CachedFSTest extends N5FSTest {
 
-	@Override protected N5Writer createN5Writer() throws IOException {
+	@Override
+	protected N5Writer createN5Writer() throws IOException {
 
 		return new N5FSWriter(tempN5PathName(), true);
 	}
 
-	@Override protected N5Writer createN5Writer(String location, GsonBuilder gson) throws IOException {
+	@Override
+	protected N5Writer createN5Writer(final String location) throws IOException {
+
+		return new N5FSWriter(location, true);
+	}
+
+	@Override
+	protected N5Writer createN5Writer(final String location, final GsonBuilder gson) throws IOException {
 
 		if (!new File(location).exists()) {
 			tmpFiles.add(location);
@@ -25,7 +38,8 @@ public class N5CachedFSTest extends N5FSTest {
 		return new N5FSWriter(location, gson, true);
 	}
 
-	@Override protected N5Reader createN5Reader(String location, GsonBuilder gson) throws IOException {
+	@Override
+	protected N5Reader createN5Reader(final String location, final GsonBuilder gson) throws IOException {
 
 		return new N5FSReader(location, gson, true);
 	}
@@ -38,7 +52,6 @@ public class N5CachedFSTest extends N5FSTest {
 		try (N5KeyValueWriter n5 = (N5KeyValueWriter) createN5Writer()) {
 			final String cachedGroup = "cachedGroup";
 			final String attributesPath = n5.attributesPath(cachedGroup);
-
 
 
 			final ArrayList<TestData<?>> tests = new ArrayList<>();
@@ -65,4 +78,43 @@ public class N5CachedFSTest extends N5FSTest {
 			Assert.assertThrows(AssertionError.class, () -> runTests(n5, tests));
 		}
 	}
+
+	@Test
+	public void cacheGroupDatasetTest() throws IOException {
+
+		final String datasetName = "dd";
+		final String groupName = "gg";
+
+		final String tmpPath = tempN5PathName();
+		try (N5KeyValueWriter w1 = (N5KeyValueWriter) createN5Writer(tmpPath);
+				N5KeyValueWriter w2 = (N5KeyValueWriter) createN5Writer(tmpPath);) {
+
+			// create a group, both writers know it exists
+			w1.createGroup(groupName);
+			assertTrue(w1.exists(groupName));
+			assertTrue(w2.exists(groupName));
+
+			// one writer removes the group
+			w2.remove(groupName);
+			assertTrue(w1.exists(groupName));	// w1's cache thinks group still exists
+			assertFalse(w2.exists(groupName));	// w2 knows group has been removed
+
+			// create a dataset
+			w1.createDataset(datasetName, dimensions, blockSize, DataType.UINT8, new RawCompression());
+			assertTrue(w1.exists(datasetName));
+			assertTrue(w2.exists(datasetName));
+
+			assertNotNull(w1.getDatasetAttributes(datasetName));
+			assertNotNull(w2.getDatasetAttributes(datasetName));
+
+			// one writer removes the data
+			w2.remove(datasetName);
+			assertTrue(w1.exists(datasetName));  // w1's cache thinks group still exists
+			assertFalse(w2.exists(datasetName)); // w2 knows group has been removed
+
+			assertNotNull(w1.getDatasetAttributes(datasetName));
+			assertNull(w2.getDatasetAttributes(datasetName));
+		}
+	}
+
 }
