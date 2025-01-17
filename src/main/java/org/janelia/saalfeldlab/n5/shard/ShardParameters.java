0e353e42fc2c6175561ead61258197580fd875c0
John Bogovic <bogovicj@janelia.hhmi.org>
2025-01-17 13:54:18 -0500

    feat: ShardParameters methods
    * shardsPerImage, blocksPerImage
    * grouping DataBlocks by shard postion
    * stream of block positions ordered by shard

--- src/main/java/org/janelia/saalfeldlab/n5/shard/ShardParameters.java
+++ src/main/java/org/janelia/saalfeldlab/n5/shard/ShardParameters.java
@@ -1,9 +1,25 @@
 package org.janelia.saalfeldlab.n5.shard;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.janelia.saalfeldlab.n5.BlockParameters;
+import org.janelia.saalfeldlab.n5.DataBlock;
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.ShardedDatasetAttributes;
 import org.janelia.saalfeldlab.n5.shard.ShardingCodec.IndexLocation;
+import org.janelia.saalfeldlab.n5.util.GridIterator;
+import org.janelia.saalfeldlab.n5.util.Position;
 
 public interface ShardParameters extends BlockParameters {
 
@@ -38,6 +54,28 @@ public interface ShardParameters extends BlockParameters {
 		return blocksPerShard;
 	}
 	
+	/**
+	 * Returns the number of blocks per dimension that tile the image.
+	 *
+	 * @return blocks per image
+	 */
+	default long[] blocksPerImage() {
+		return IntStream.range(0, getNumDimensions()).mapToLong(i -> {
+			return (long) Math.ceil(getDimensions()[i] / getBlockSize()[i]);
+		}).toArray();
+	}
+
+	/**
+	 * Returns the number of shards per dimension that tile the image.
+	 *
+	 * @return shards per image
+	 */
+	default long[] shardsPerImage() {
+		return IntStream.range(0, getNumDimensions()).mapToLong(i -> {
+			return (long)Math.ceil(getDimensions()[i] / getShardSize()[i]);
+		}).toArray();
+	}
+
 	/**
 	 * Given a block's position relative to the array, returns the position of the shard containing that block relative to the shard grid.
 	 *
@@ -92,7 +130,6 @@ public interface ShardParameters extends BlockParameters {
 
 		return blockShardPos;
 	}
-	
 
 	/**
 	 * Given a block's position relative to a shard, returns its position in pixels
@@ -134,13 +171,59 @@ public interface ShardParameters extends BlockParameters {
 
 		return blockImagePos;
 	}
+
+	default Map<Position, List<long[]>> groupBlockPositions(final List<long[]> blockPositions) {
+
+		final TreeMap<Position, List<long[]>> map = new TreeMap<>();
+		for( final long[] blockPos : blockPositions ) {
+			Position shardPos = Position.wrap(getShardPositionForBlock(blockPos));
+			if( !map.containsKey(shardPos)) {
+				map.put(shardPos, new ArrayList<>());
+			}
+			map.get(shardPos).add(blockPos);
+		}
+
+		return map;
+	}
 	
+	default <T> Map<Position, List<DataBlock<T>>> groupBlocks(final List<DataBlock<T>> blocks) {
+
+		// figure out how to re-use groupBlockPositions here?
+		final TreeMap<Position, List<DataBlock<T>>> map = new TreeMap<>();
+		for (final DataBlock<T> block : blocks) {
+			Position shardPos = Position.wrap(getShardPositionForBlock(block.getGridPosition()));
+			if (!map.containsKey(shardPos)) {
+				map.put(shardPos, new ArrayList<>());
+			}
+			map.get(shardPos).add(block);
+		}
+
+		return map;
+	}
+
 	/**
 	 * @return the number of blocks per shard
 	 */
 	default long getNumBlocks() {
 
 		return Arrays.stream(getBlocksPerShard()).reduce(1, (x, y) -> x * y);
+	}
+
+	default Stream<long[]> blockPositions() {
+
+		final int[] blocksPerShard = getBlocksPerShard();
+		return toStream( new GridIterator(shardsPerImage()))
+				.flatMap( shardPosition -> {
+					final int nd = getNumDimensions();
+					final long[] min = getBlockPositionFromShardPosition(shardPosition, new long[nd]);
+					return toStream(new GridIterator(GridIterator.int2long(blocksPerShard), min));
+				});
+	}
+
+	static <T> Stream<T> toStream( final Iterator<T> it ) {
+		return StreamSupport.stream( Spliterators.spliteratorUnknownSize(
+				  it, Spliterator.ORDERED),
+		          false);
 	}
 
 }
--- src/test/java/org/janelia/saalfeldlab/n5/shard/ShardPropertiesTests.java
+++ src/test/java/org/janelia/saalfeldlab/n5/shard/ShardPropertiesTests.java
@@ -2,15 +2,18 @@ package org.janelia.saalfeldlab.n5.shard;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.janelia.saalfeldlab.n5.DataType;
 import org.janelia.saalfeldlab.n5.ShardedDatasetAttributes;
 import org.janelia.saalfeldlab.n5.codec.Codec;
 import org.janelia.saalfeldlab.n5.codec.DeterministicSizeCodec;
 import org.janelia.saalfeldlab.n5.shard.ShardingCodec.IndexLocation;
+import org.janelia.saalfeldlab.n5.util.Position;
 import org.junit.Test;
 
 public class ShardPropertiesTests {
@@ -84,7 +87,32 @@ public class ShardPropertiesTests {
 		}
 		assertEquals(16,i);
 		assertArrayEquals(new long[]{7,7}, p);
+	}
 
+	@Test
+	public void testShardGrouping() {
+
+		final long[] arraySize = new long[]{8, 12};
+		final int[] shardSize = new int[]{4, 6};
+		final int[] blkSize = new int[]{2, 3};
+
+		final ShardedDatasetAttributes attrs = new ShardedDatasetAttributes(
+				arraySize,
+				shardSize,
+				blkSize,
+				DataType.UINT8,
+				new Codec[]{},
+				new DeterministicSizeCodec[]{},
+				IndexLocation.END);
+
+		List<long[]> blockPositions = attrs.blockPositions().collect(Collectors.toList());
+		final Map<Position, List<long[]>> result = attrs.groupBlockPositions(blockPositions);
+
+		// there are four shards in this image
+		assertEquals(4, result.keySet().size());
+
+		// there are four blocks per shard in this image
+		result.values().stream().forEach( x -> assertEquals(4, x.size()));
 	}
 
 }
