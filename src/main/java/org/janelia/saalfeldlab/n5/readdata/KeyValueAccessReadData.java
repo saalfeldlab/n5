59d53bebf3b2ab47a2618262ebb2812a185baa45
tpietzsch <tobias.pietzsch@gmail.com>
2025-01-31 22:38:22 +0100

    Lean more on ReadData
    * Move ReadData etc to separate classes
    * Add ReadData.writeTo(OutputStream)
    * Add EncodedReadData that wraps a ReadData and an OutputStreamEncoder
    * Compression (BytesCodec) can encode ReadData. (This might happen
      immediately or later when the ReadData is written to OutputStream).

---
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/KeyValueAccessReadData.java
@@ -0,0 +1,43 @@
+package org.janelia.saalfeldlab.n5.readdata;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.janelia.saalfeldlab.n5.KeyValueAccess;
+import org.janelia.saalfeldlab.n5.LockedChannel;
+
+public class KeyValueAccessReadData extends AbstractInputStreamReadData {
+
+	private final KeyValueAccess keyValueAccess;
+	private final String normalPath;
+
+	public KeyValueAccessReadData(final KeyValueAccess keyValueAccess, final String normalPath) {
+		this.keyValueAccess = keyValueAccess;
+		this.normalPath = normalPath;
+	}
+
+	/**
+	 * Open a {@code InputStream} on this data.
+	 * <p>
+	 * This will open a {@code LockedChannel} on the underlying {@code
+	 * KeyValueAccess}. Make sure to {@code close()} the returned {@code
+	 * InputStream} to release the underlying {@code LockedChannel}.
+	 *
+	 * @return an InputStream on this data
+	 *
+	 * @throws IOException
+	 * 		if any I/O error occurs
+	 */
+	@Override
+	public InputStream inputStream() throws IOException {
+		final LockedChannel channel = keyValueAccess.lockForReading(normalPath);
+		return new FilterInputStream(channel.newInputStream()) {
+
+			@Override
+			public void close() throws IOException {
+				in.close();
+				channel.close();
+			}
+		};
+	}
+}
