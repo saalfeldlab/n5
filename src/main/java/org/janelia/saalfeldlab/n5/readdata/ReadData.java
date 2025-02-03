4fd4723318f78195d10689ff35d9180aa1ce6fd9
tpietzsch <tobias.pietzsch@gmail.com>
2025-02-03 17:53:31 +0100

    fix javadoc error

--- src/main/java/org/janelia/saalfeldlab/n5/readdata/ReadData.java
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/ReadData.java
@@ -140,9 +140,6 @@ public interface ReadData {
 	 * 		OutputStreamEncoder to use for encoding
 	 *
 	 * @return encoded ReadData
-	 *
-	 * @throws IOException
-	 * 		if any I/O error occurs
 	 */
 	default ReadData encode(OutputStreamEncoder encoder) {
 		return new EncodedReadData(this, encoder);
