65345d9f1e0ae308c10039eb95102081c6d529d7
John Bogovic <bogovicj@janelia.hhmi.org>
2025-05-23 17:10:48 -0400

    doc: SplittableReadData.slice

--- src/main/java/org/janelia/saalfeldlab/n5/readdata/SplittableReadData.java
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/SplittableReadData.java
@@ -10,8 +10,22 @@ public interface SplittableReadData extends ReadData {
 		return slice(0, length);
 	}
 
+	/**
+	 * Returns a new {@link ReadData} representing a slice, or subset
+	 * of this ReadData.
+	 *
+	 * @param offset the offset relative to this
+	 * @param length of the returned ReadData
+	 * @return a slice
+	 * @throws IOException an exception
+	 */
 	ReadData slice(final long offset, final long length) throws IOException;
 
-	// TODO do we want this? how should it work?
+	/*
+	 * TODO do we want this? how should it work?
+	 * this could be useful for infinite data, or data of unknown length?
+	 * 
+	 * tail below would be equivalent to slice(pivot, -1)
+	 */
 	Pair<ReadData,ReadData> split(final long pivot) throws IOException;
 }
