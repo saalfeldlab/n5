5e3401623836481de743cb2ea5b5704fc892ade6
tpietzsch <tobias.pietzsch@gmail.com>
2025-07-28 11:32:30 -0400

    Use private instead of protected
    until necessary...

--- src/main/java/org/janelia/saalfeldlab/n5/DatasetAttributes.java
+++ src/main/java/org/janelia/saalfeldlab/n5/DatasetAttributes.java
@@ -6,13 +6,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -66,9 +66,8 @@ public class DatasetAttributes implements Serializable {
 	private final int[] blockSize;
 	private final DataType dataType;
 
-	protected final ArrayCodec arrayCodec;
-	protected final BytesCodec[] byteCodecs;
-	
+	private final ArrayCodec arrayCodec;
+	private final BytesCodec[] byteCodecs;
 
 	public DatasetAttributes(
 			final long[] dimensions,
@@ -107,7 +106,7 @@ public class DatasetAttributes implements Serializable {
 
 		this.arrayCodec.initialize(this, byteCodecs);
 	}
-	
+
 	public DatasetAttributes(
 			final long[] dimensions,
 			final int[] blockSize,
