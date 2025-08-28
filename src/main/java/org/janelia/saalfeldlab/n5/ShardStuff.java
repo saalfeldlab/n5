2f2e2fe0a0c46fb56d4e0fd7eeac34d865f112ce
tpietzsch <tobias.pietzsch@gmail.com>
2025-08-28 11:24:29 +0200

    WIP BlockCodecInfo

--- src/main/java/org/janelia/saalfeldlab/n5/ShardStuff.java
+++ src/main/java/org/janelia/saalfeldlab/n5/ShardStuff.java
@@ -1,6 +1,12 @@
 package org.janelia.saalfeldlab.n5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.janelia.saalfeldlab.n5.N5Exception.N5IOException;
+import org.janelia.saalfeldlab.n5.codec.CodecInfo;
+import org.janelia.saalfeldlab.n5.codec.DataCodec;
+import org.janelia.saalfeldlab.n5.codec.DataCodecInfo;
 import org.janelia.saalfeldlab.n5.readdata.ReadData;
 
 public class ShardStuff {
@@ -75,17 +81,91 @@ public class ShardStuff {
 	}
 
 
-	public interface BlockCodec<T extends Block> {
+	public interface BlockCodec<B extends Block> {
 
-		ReadData encode(T block) throws N5IOException;
+		ReadData encode(B block) throws N5IOException;
 
-		T decode(ReadData readData, long[] gridPosition) throws N5IOException;
+		B decode(ReadData readData, long[] gridPosition) throws N5IOException;
 	}
 
 
 	public interface DataBlockCodec<T> extends BlockCodec<DataBlock<T>> {
 	}
 
+	public static class BlockCodecs<T> {
+
+		private final List<BlockCodec<?>> shardCodecs = new ArrayList<>();
+		private final DataBlockCodec<T> dataBlockCodec;
+		private final int[] dataBlockSize;
+
+		public BlockCodecs(final DataBlockCodec<T> dataBlockCodec, final int[] dataBlockSize) {
+			this.dataBlockCodec = dataBlockCodec;
+			this.dataBlockSize = dataBlockSize;
+		}
+
+		public BlockCodecs(final BlockCodec<?> first, final BlockCodecs<T> others) {
+			shardCodecs.add(first);
+			shardCodecs.addAll(others.shardCodecs);
+			dataBlockCodec = others.dataBlockCodec;
+			dataBlockSize = others.dataBlockSize;
+		}
+
+		public int[] getDataBlockSize() {
+			return dataBlockSize;
+		}
+	}
+
+	public interface BlockCodecInfo extends CodecInfo {
+
+		/**
+		 * Chunk size of the elements in this block.
+		 * That is, (1, ...) for DataBlockCodecInfo, respectively inner block size for ShardCodecInfo.
+		 */
+		int[] getChunkSize();
+
+		/**
+		 * Nested BlockCodec.
+		 * ({@code null} for DataBlockCodec.
+		 */
+		BlockCodecInfo getInnerBlockCodecInfo();
+
+		DataCodecInfo[] getInnerDataCodecInfos();
+
+		<B extends Block> BlockCodec<B> create(int[] blockSize, DataCodecInfo... codecs);
+
+		default <T> BlockCodecs<T> createRecursive(DataType dataType, int[] blockSize, DataCodecInfo... codecs)
+		{
+			final BlockCodecs<T> blockCodecs = getInnerBlockCodecInfo().createRecursive(
+					dataType, getChunkSize(),
+					getInnerDataCodecInfos());
+			final BlockCodec<Block> shardCodec = create(blockSize, codecs);
+			return new BlockCodecs<>(shardCodec, blockCodecs);
+		}
+	}
+
+	private static DataCodec[] instantiate(final DataCodecInfo... codecInfos) {
+		final DataCodec[] codecs = new DataCodec[codecInfos.length];
+		Arrays.setAll(codecs, i -> codecInfos[i].create());
+		return codecs;
+	}
+
+	public interface DataBlockCodecInfo extends BlockCodecInfo {
+
+		@Override
+		default <B extends Block> BlockCodec<B> create(int[] blockSize, DataCodecInfo... codecs) {
+			// TODO: Fix BlockCodec hierarchy. It's a bad sign that we have to
+			//       @Override this here. Maybe AbstractBlockCodecInfo extended by
+			//       (Data)BlockCodecInfo and ShardCodecInfo
+			throw new UnsupportedOperationException();
+		}
+
+		<T> DataBlockCodec<T> create(DataType dataType, int[] blockSize, DataCodecInfo... codecs);
+
+		@Override
+		default <T> BlockCodecs<T> createRecursive(DataType dataType, int[] blockSize, DataCodecInfo... codecs) {
+			return new BlockCodecs<>(create(dataType, blockSize, codecs), blockSize);
+		}
+	}
 
 	static class N5ReaderImpl {
 
