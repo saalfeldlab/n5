6fb023c95250419816385601834a32b8c73608f0
tpietzsch <tobias.pietzsch@gmail.com>
2025-10-21 16:49:31 +0200

    refactor: Make DefaultSegmentLocation a local class

--- src/main/java/org/janelia/saalfeldlab/n5/readdata/segment/SegmentLocation.java
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/segment/SegmentLocation.java
@@ -19,6 +19,52 @@ public interface SegmentLocation {
 	}
 
 	static SegmentLocation at(final long offset, final long length) {
+
+		class DefaultSegmentLocation implements SegmentLocation {
+
+			private final long offset;
+			private final long length;
+
+			public DefaultSegmentLocation(final long offset, final long length) {
+				this.offset = offset;
+				this.length = length;
+			}
+
+			@Override
+			public long offset() {
+				return offset;
+			}
+
+			@Override
+			public long length() {
+				return length;
+			}
+
+			@Override
+			public final boolean equals(final Object o) {
+				if (!(o instanceof DefaultSegmentLocation))
+					return false;
+
+				final DefaultSegmentLocation that = (DefaultSegmentLocation) o;
+				return offset == that.offset && length == that.length;
+			}
+
+			@Override
+			public int hashCode() {
+				int result = Long.hashCode(offset);
+				result = 31 * result + Long.hashCode(length);
+				return result;
+			}
+
+			@Override
+			public String toString() {
+				return "Range{" +
+						"offset=" + offset +
+						", length=" + length +
+						'}';
+			}
+		}
+
 		return new DefaultSegmentLocation(offset, length);
 	}
 }
