0b8ffed749064807ea8ecd4bbf97f672a8546b1f
tpietzsch <tobias.pietzsch@gmail.com>
2025-10-22 22:06:37 +0200

    javadoc

--- src/main/java/org/janelia/saalfeldlab/n5/readdata/segment/Segment.java
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/segment/Segment.java
@@ -5,5 +5,17 @@ package org.janelia.saalfeldlab.n5.readdata.segment;
  */
 public interface Segment {
 
+	/**
+	 * Returns the {@code SegmentedReadData} on which this segment is originally
+	 * defined. (The segment is tracked through slices and concatenations, but
+	 * the source will remain the same.)
+	 * <p>
+	 * This is mostly just used internally to make {@code SegmentedReadData}
+	 * implementations easier. The only real use for {@code source()} outside of
+	 * that is to get a {@code ReadData} containing exactly this segment, using
+	 * {@code segment.source().slice(segment)}.
+	 *
+	 * @return the {@code SegmentedReadData} on which this segment is originally defined
+	 */
 	SegmentedReadData source();
 }
