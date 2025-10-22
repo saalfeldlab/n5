d987cd461608fce2e3415c00175cefc63d8cc3f7
tpietzsch <tobias.pietzsch@gmail.com>
2025-10-22 12:16:40 +0200

    augment and document concatenate test

--- src/test/java/org/janelia/saalfeldlab/n5/readdata/segment/ConcatenatedReadDataTest.java
+++ src/test/java/org/janelia/saalfeldlab/n5/readdata/segment/ConcatenatedReadDataTest.java
@@ -1,11 +1,13 @@
 package org.janelia.saalfeldlab.n5.readdata.segment;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.janelia.saalfeldlab.n5.readdata.Range;
 import org.janelia.saalfeldlab.n5.readdata.ReadData;
+import org.janelia.saalfeldlab.n5.readdata.segment.SegmentedReadData.SegmentsAndData;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,70 +15,181 @@ import static org.junit.Assert.assertEquals;
 
 public class ConcatenatedReadDataTest {
 
-	private ReadData readData;
-
-	private ReadData readDataUnknownLength;
+	private final byte[] data = new byte[100];
 
 	@Before
-	public void createReadData() {
+	public void fillData() {
 
-		final byte[] data = new byte[100];
 		for (int i = 0; i < data.length; i++) {
 			data[i] = (byte) i;
 		}
-		readData = ReadData.from(data);
-		readDataUnknownLength = ReadData.from(new ByteArrayInputStream(data));
 	}
 
-	@Test
-	public void testConcatenate() {
+	/**
+	 * Create a SegmentedReadData with segments at (10,l=30), (0,l=10), and (40,l=20).
+	 * The returned ReadData knows its length.
+	 * <p>
+	 * <pre>
+	 * [0.....10.....20.....30.....40.....50.....60.....70.....80.....90.....]
+	 * [(-s1-)(---------s0--------)(-----s2-----)............................]
+	 * </pre>
+	 */
+	private SegmentsAndData createKnownLength() {
 
-		final Range[] locations = {
+		return SegmentedReadData.wrap(
+				ReadData.from(data),
 				Range.at(10, 30),
 				Range.at(0, 10),
-				Range.at(40, 20)};
-		final SegmentedReadData.SegmentsAndData segmentsAndData0 = SegmentedReadData.wrap(readData, locations);
-		final SegmentedReadData r0 = segmentsAndData0.data();
-		final List<Segment> segments0 = segmentsAndData0.segments();
+				Range.at(40, 20));
+	}
 
-		final SegmentedReadData r1 = SegmentedReadData.wrap(readDataUnknownLength);
-		assertEquals(1, r1.segments().size());
-		final List<Segment> segments1 = Collections.singletonList(r1.segments().get(0));
+	/**
+	 * Create a SegmentedReadData with one segment spanning it completely.
+	 * The returned ReadData doesn't know its length.
+	 * <p>
+	 * <pre>
+	 * [0.....10.....20.....30.....40.....50.....60.....70.....80.....90.....]
+	 * [(------------------------------s3-----------------------------------)]
+	 * </pre>
+	 */
+	private SegmentsAndData createUnknownLength() {
+
+		final SegmentedReadData srd = SegmentedReadData.wrap(
+				ReadData.from(
+						new ByteArrayInputStream(data)));
+
+		return new SegmentsAndData() {
+
+			@Override
+			public List<Segment> segments() {
+				return Collections.singletonList(srd.segments().get(0));
+			}
+
+			@Override
+			public SegmentedReadData data() {
+				return srd;
+			}
+		};
+	}
+
+
+	/**
+	 * Create slices of known length and unknown length SegmentedReadData, and concatenate.
+	 * Take slice (0,l=40)
+	 * <p>
+	 * <pre>
+	 *
+	 * KNOWN_LENGTH:
+	 * [0.....10.....20.....30.....40.....50.....60.....70.....80.....90.....]
+	 * [(-s1-)(---------s0--------)(-----s2-----)............................]
+	 *
+	 * SLICE_0
+	 * [(-s1-)(---------s0--------)]
+	 *
+	 *                            SLICE_1
+	 *                            [(-----s2-----)]
+	 *
+	 * UNKNOWN_LENGTH:
+	 * [0.....10.....20.....30.....40.....50.....60.....70.....80.....90.....]
+	 * [(------------------------------s3-----------------------------------)]
+	 *
+	 * CONCATENATED:
+	 * [-------- SLICE_0 ----------][- UNKNOWN_LENGTH -][-- SLICE_1 ---]
+	 * [(-s1-)(---------s0--------)][(--...--s3--...--)][(-----s2-----)]
+	 *
+	 * </pre>
+	 */
+	private SegmentsAndData createConcatenate() {
+
+		final SegmentsAndData segmentsAndData0 = createKnownLength();
+		final SegmentedReadData r0 = segmentsAndData0.data();
+
+		final SegmentsAndData segmentsAndData1 = createUnknownLength();
+		final SegmentedReadData r1 = segmentsAndData1.data();
+
+		final List<Segment> segments = new ArrayList<>();
+		segments.addAll(segmentsAndData0.segments());
+		segments.addAll(segmentsAndData1.segments());
 
 		final List<SegmentedReadData> datas = new ArrayList<>();
 		datas.add(r0.slice(0,40));
 		datas.add(r1);
-		datas.add(r0.slice(segments0.get(2)));
-		final SegmentedReadData c = SegmentedReadData.concatenate(datas);
+		datas.add(r0.slice(segments.get(2)));
+		final SegmentedReadData concatenated = SegmentedReadData.concatenate(datas);
 
-		// TODO: create individual tests:
-		//       Both materialize() and writeTo(OutputStream) should ensure that all SegmentLocations are known
-		//       Otherwise we expect IllegalStateException
-		c.materialize();
-//		c.writeTo(new ByteArrayOutputStream());
+		return new SegmentsAndData() {
 
-//		for (Segment segment : segments0) {
-//			System.out.println("c.location(segment) = " + c.location(segment));
-//		}
-//		System.out.println("c.location(segment) = " + c.location(segments1.get(0)));
+			@Override
+			public List<Segment> segments() {
+				return segments;
+			}
 
-		final Range l1 = c.location(segments0.get(1));
-		assertEquals(0, l1.offset());
-		assertEquals(10, l1.length());
-
-		final Range l0 = c.location(segments0.get(0));
-		assertEquals(10, l0.offset());
-		assertEquals(30, l0.length());
-
-		final Range l3 = c.location(segments1.get(0));
-		assertEquals(40, l3.offset());
-		assertEquals(100, l3.length());
-
-		final Range l2 = c.location(segments0.get(2));
-		assertEquals(140, l2.offset());
-		assertEquals(20, l2.length());
-
-
+			@Override
+			public SegmentedReadData data() {
+				return concatenated;
+			}
+		};
 	}
 
+	/**
+	 * Check that segments in the concatenated ReadData are at the expected locations.
+	 * The segments should be laid out like this:
+	 * <p>
+	 * <pre>
+	 * CONCATENATED:
+	 * [0.....10.....20.....30.....40...             ...140.....150.....]
+	 * [-------- SLICE_0 ----------][- UNKNOWN_LENGTH -][--- SLICE_1 ---]
+	 * [(-s1-)(---------s0--------)][(--...--s3--...--)][(------s2-----)]
+	 * </pre>
+	 * <p>
+	 */
+	private static void checkSegmentLocations(final SegmentsAndData segmentsAndData) {
+		checkSegmentRange(segmentsAndData,1, Range.at(0, 10));
+		checkSegmentRange(segmentsAndData,0, Range.at(10, 30));
+		checkSegmentRange(segmentsAndData,3, Range.at(40, 100));
+		checkSegmentRange(segmentsAndData,2, Range.at(140, 20));
+	}
+
+	private static void checkSegmentRange(final SegmentsAndData data, final int segmentIndex, final Range expectedLocation)
+	{
+		final Range location = data.data().location(data.segments().get(segmentIndex));
+		assertEquals(expectedLocation.offset(), location.offset());
+		assertEquals(expectedLocation.length(), location.length());
+	}
+
+	// A concatenated ReadData containing unknown-length parts requires
+	// either materialize() or writeTo(OutputStream) to make those lengths
+	// known. Otherwise, trying to get segment locations from the
+	// concatenated ReadData fails with an IllegalStateException.
+
+	@Test(expected = IllegalStateException.class)
+	public void testConcatenateUnmaterialized() {
+
+		final SegmentsAndData segmentsAndData = createConcatenate();
+		final SegmentedReadData c = segmentsAndData.data();
+		final List<Segment> s = segmentsAndData.segments();
+		System.out.println(c.location(s.get(0)));
+	}
+
+	// Calling materialize() on the concatenated ReadData makes sure that all
+	// segment offsets are known.
+
+	@Test
+	public void testConcatenateMaterialize() {
+
+		final SegmentsAndData segmentsAndData = createConcatenate();
+		segmentsAndData.data().materialize();
+		checkSegmentLocations(segmentsAndData);
+	}
+
+	// Calling writeTo(OutputStream) on the concatenated ReadData makes sure
+	// that all segment offsets are known.
+
+	@Test
+	public void testConcatenateWriteTo() {
+
+		final SegmentsAndData segmentsAndData = createConcatenate();
+		segmentsAndData.data().writeTo(new ByteArrayOutputStream());
+		checkSegmentLocations(segmentsAndData);
+	}
 }
