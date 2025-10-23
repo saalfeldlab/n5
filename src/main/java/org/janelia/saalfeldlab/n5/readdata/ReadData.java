2e1a2b6cde42388fc9e3399da0e5a2dd7668c11d
tpietzsch <tobias.pietzsch@gmail.com>
2025-10-23 11:14:48 +0200

    Add optional ReadData.prefetch(...) method
    The default implementation does nothing

--- src/main/java/org/janelia/saalfeldlab/n5/readdata/ReadData.java
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/ReadData.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.Collection;
 import org.janelia.saalfeldlab.n5.N5Exception.N5IOException;
 import org.janelia.saalfeldlab.n5.codec.DataCodec;
 
@@ -220,6 +221,20 @@ public interface ReadData {
 		} catch (IOException e) {
 			throw new N5IOException(e);
 		}
+	}
+
+	/**
+	 * Indicates that the given slices will be subsequently read.
+	 * {@code ReadData} implementations (optionally) may take steps to prepare
+	 * for these subsequent slices.
+	 *
+	 * @param ranges
+	 * 		slice ranges to prefetch
+	 *
+	 * @throws N5IOException
+	 * 		if any I/O error occurs
+	 */
+	default void prefetch(final Collection<? extends Range> ranges) throws N5IOException {
 	}
 
 	// ------------- Encoding / Decoding ----------------
