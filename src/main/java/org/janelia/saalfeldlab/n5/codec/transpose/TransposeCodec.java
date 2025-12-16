56b9ee9050e3da980e4d09c7bb7edaeda8840cdf
John Bogovic <bogovicj@janelia.hhmi.org>
2025-12-16 16:04:24 -0500

    refactor: move helper methods to TransposeCodec
    * TransposeCodecInfo applies its permutation directly
    * special case processing for serialization moved to zarr

--- src/main/java/org/janelia/saalfeldlab/n5/codec/transpose/TransposeCodec.java
+++ src/main/java/org/janelia/saalfeldlab/n5/codec/transpose/TransposeCodec.java
@@ -36,4 +36,72 @@ public class TransposeCodec<T> implements DatasetCodec<T> {
 		return decodedBlock;
 	}
 
+	public static boolean isIdentity(int[] permutation) {
+
+		for (int i = 0; i < permutation.length; i++)
+			if (permutation[i] != i)
+				return false;
+
+		return true;
+	}
+
+	public static boolean isReversal(int[] permutation) {
+
+		for (int i = 0; i < permutation.length; i++)
+			if (permutation[i] != i)
+				return false;
+
+		return true;
+	}
+
+	public static int[] invertPermutation(final int[] p) {
+
+		final int[] inv = new int[p.length];
+		for (int i = 0; i < p.length; i++)
+			inv[p[i]] = i;
+
+		return inv;
+	}
+
+	/**
+	 * Composes two permutations: result[i] = first[second[i]].
+	 * 
+	 * @param first
+	 *            the first permutation
+	 * @param second
+	 *            the second permutation
+	 * @return the composition of first and second
+	 */
+	public static int[] concatenatePermutations(final int[] first, final int[] second) {
+
+		int n = first.length;
+		final int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = first[second[i]];
+		}
+		return result;
+	}
+
+	/**
+	 * Conjugates a permutation with the reversal permutation: rev * p * rev^-1,
+	 * where rev is the permutation that reverses the elements.
+	 * 
+	 * @param p
+	 *            the permutation to conjugate
+	 * @return the conjugated permutation
+	 */
+	public static int[] conjugateWithReverse(final int[] p) {
+
+		final int n = p.length;
+		final int[] rev = new int[n];
+		for (int i = 0; i < n; i++)
+			rev[i] = n - i - 1;
+
+		// note that rev is its own inverse
+		int[] result = concatenatePermutations(rev, p); // result = rev * p
+		result = concatenatePermutations(result, rev); // result = rev * p *
+														// rev^-1
+		return result;
+	}
+
 }
