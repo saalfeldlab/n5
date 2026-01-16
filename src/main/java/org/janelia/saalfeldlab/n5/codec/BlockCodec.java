3c7d6080d21d59af6033991cccfc80d68de60ab3
John Bogovic <bogovicj@janelia.hhmi.org>
2026-01-16 16:39:33 -0500

    doc: BlockCodec encode / decode

--- src/main/java/org/janelia/saalfeldlab/n5/codec/BlockCodec.java
+++ src/main/java/org/janelia/saalfeldlab/n5/codec/BlockCodec.java
@@ -30,7 +30,6 @@ package org.janelia.saalfeldlab.n5.codec;
 
 import org.janelia.saalfeldlab.n5.DataBlock;
 import org.janelia.saalfeldlab.n5.N5Exception.N5IOException;
-import org.janelia.saalfeldlab.n5.RawCompression;
 import org.janelia.saalfeldlab.n5.readdata.ReadData;
 
 /**
@@ -41,8 +40,45 @@ import org.janelia.saalfeldlab.n5.readdata.ReadData;
  */
 public interface BlockCodec<T> {
 
+	/**
+	 * Serializes a {@link DataBlock} into a {@link ReadData} representation for
+	 * storage.
+	 * <p>
+	 * The encoding process serializes the block's data, applies any configured
+	 * compression, and may include metadata depending on the codec
+	 * implementation.
+	 *
+	 * @param dataBlock
+	 *            the data block to encode
+	 *
+	 * @return serialized representation of the data block
+	 *
+	 * @throws N5IOException
+	 *             if encoding or compression fails
+	 *
+	 * @see #decode(ReadData, long[])
+	 */
 	ReadData encode(DataBlock<T> dataBlock) throws N5IOException;
 
+	/**
+	 * Deserializes a {@link DataBlock} from its {@link ReadData}
+	 * representation.
+	 * <p>
+	 * Reverses the encoding process by decompressing (if needed) and
+	 * deserializing the data.
+	 *
+	 * @param readData
+	 *            the serialized data to decode
+	 * @param gridPosition
+	 *            position of this block on the block grid (level 0 coordinates)
+	 *
+	 * @return reconstructed data block with deserialized data and grid position
+	 *
+	 * @throws N5IOException
+	 *             if decoding, decompression, or data validation fails
+	 *
+	 * @see #encode(DataBlock)
+	 */
 	DataBlock<T> decode(ReadData readData, long[] gridPosition) throws N5IOException;
 
 	/**
