ac62c87e444f969655f398dbae9932c19060f47b
tpietzsch <tobias.pietzsch@gmail.com>
2026-02-02 21:16:29 +0100

    Add SliceTrackingLazyRead
    This is in preparation for implementing prefetch() for cloud storage
    backends...

---
+++ src/main/java/org/janelia/saalfeldlab/n5/readdata/prefetch/Slices.java
@@ -0,0 +1,116 @@
+package org.janelia.saalfeldlab.n5.readdata.prefetch;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.janelia.saalfeldlab.n5.readdata.Range;
+
+class Slices {
+
+	private Slices() {
+		// utility class. should not be instantiated.
+	}
+
+	/**
+	 * In an ordered list of {@code slices}, find a slice that completely contains the given range.
+	 * <p>
+	 * Pre-conditions:
+	 * <ol>
+	 * <li>Slices are ordered by offset.</li>
+	 * <li>If two slices overlap, no slice is fully contained within the other.
+	 *     (Therefore, if {@code a.offset < b.offset} then {@code a.end < b.end}.)</li>
+	 * </ol>
+	 *
+	 * @param slices
+	 * 		ordered list of slices
+	 * @param offset
+	 * 		start of the range to cover
+	 * @param length
+	 * 		length of the range to cover
+	 *
+	 * @return a slice that completely contains the requested range, or {@code null} if no such slice exists
+	 */
+	static <T extends Range> T findContainingSlice(final List<T> slices, final long offset, final long length) {
+
+		// Find the slice with the largest slice.offset <= offset.
+		final int i = Collections.binarySearch(slices, Range.at(offset, 0), Comparator.comparingLong(Range::offset));
+
+		// Largest index of a slice with slice.offset <= offset.
+		final int index = i < 0 ? -i - 2 : i;
+		if (index < 0) {
+			// We find no overlapping slice, because
+			// slices[0].offset is already too large.
+			return null;
+		}
+
+		final T slice = slices.get(index);
+		if (slice.end() < offset + length) {
+			return null;
+		}
+
+		return slice;
+	}
+
+	/**
+	 * In an ordered list of {@code slices}, find a slice that completely contains the given range.
+	 * <p>
+	 * Pre-conditions:
+	 * <ol>
+	 * <li>Slices are ordered by offset.</li>
+	 * <li>If two slices overlap, no slice is fully contained within the other.
+	 *     (Therefore, if {@code a.offset < b.offset} then {@code a.end < b.end}.)</li>
+	 * </ol>
+	 *
+	 * @param slices
+	 * 		ordered list of slices
+	 * @param range
+	 * 		range to cover
+	 *
+	 * @return a slice that completely contains the requested range, or {@code null} if no such slice exists
+	 */
+	static <T extends Range> T findContainingSlice(final List<T> slices, final Range range) {
+		return findContainingSlice(slices, range.offset(), range.length());
+	}
+
+	/**
+	 * Add a new {@code slice} to the {@code slice} list.
+	 * <p>
+	 * Note, that the new {@code slice} is expected to not be fully contained in
+	 * an existing slice!
+	 * <p>
+	 * Pre/post-conditions:
+	 * <ol>
+	 * <li>Slices are ordered by offset.</li>
+	 * <li>If two slices overlap, no slice is fully contained within the other.
+	 *     (Therefore, if {@code a.offset < b.offset} then {@code a.end < b.end}.)</li>
+	 * </ol>
+	 * <p>
+	 * The new {@code slice} will be inserted into the list at the correct position
+	 * (such that {@code slices} remains ordered by slice offset), and all existing
+	 * slices that are fully contained in the new {@code slice} will be removed.
+	 *
+	 * @param slices
+	 * 		ordered list of slices
+	 * @param slice
+	 * 		slice to be inserted
+	 */
+	static <T extends Range> void addSlice(final List<T> slices, final T slice) {
+
+		final int i = Collections.binarySearch(slices, slice, Comparator.comparingLong(Range::offset));
+		final int from = i < 0 ? -i - 1 : i;
+
+		int to = from;
+		while (to < slices.size() && slices.get(to).end() <= slice.end()) {
+			++to;
+		}
+
+		if (from == to) {
+			// empty range: just insert
+			slices.add(from, slice);
+		} else {
+			// overwrite the first element in range, remove the rest
+			slices.set(from, slice);
+			slices.subList(from + 1, to).clear();
+		}
+	}
+}
---
+++ src/test/java/org/janelia/saalfeldlab/n5/readdata/prefetch/SlicesTest.java
@@ -0,0 +1,134 @@
+package org.janelia.saalfeldlab.n5.readdata.prefetch;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.janelia.saalfeldlab.n5.readdata.Range;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SlicesTest {
+
+	private List<Range> createSlices(final long[] offsets, final long[] lengths) {
+		final List<Range> slices = new ArrayList<>();
+		for (int i = 0; i < offsets.length; ++i) {
+			slices.add(Range.at(offsets[i], lengths[i]));
+		}
+		return slices;
+	}
+
+	@Test
+	public void testFindContaining() {
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (2,6)    [-----------]
+		// (6,4)            [---------]
+		// (8,6)                [-----------]
+
+		final List<Range> slices = createSlices(
+				new long[] {2, 6, 8},
+				new long[] {6, 4, 6});
+		Range slice;
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (1,1)  [-]
+		slice = Slices.findContainingSlice(slices, 1, 1);
+		assertEquals(null, slice);
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (2,1)    [-]
+		slice = Slices.findContainingSlice(slices, 2, 1);
+		assertEquals(2, slice.offset());
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (2,6)    [-----------]
+		slice = Slices.findContainingSlice(slices, 2, 6);
+		assertEquals(2, slice.offset());
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (2,7)    [-------------]
+		slice = Slices.findContainingSlice(slices, 2, 7);
+		assertEquals(null, slice);
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (6,4)            [-------]
+		slice = Slices.findContainingSlice(slices, 6, 4);
+		assertEquals(6, slice.offset());
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (8,2)                [---]
+		slice = Slices.findContainingSlice(slices, 8, 2);
+		assertEquals(8, slice.offset());
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (12,2)                       [---]
+		slice = Slices.findContainingSlice(slices, 12, 2);
+		assertEquals(8, slice.offset());
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (12,3)                       [-----]
+		slice = Slices.findContainingSlice(slices, 12, 3);
+		assertEquals(null, slice);
+
+		//       0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (14,1)                           [-]
+		slice = Slices.findContainingSlice(slices, 14, 1);
+		assertEquals(null, slice);
+	}
+
+
+	@Test
+	public void testAddSlice() {
+
+		//        0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (2,6)     [-----------]
+		// (6,4)             [---------]
+		// (8,6)                 [-----------]
+		final List<Range> initial = createSlices(
+				new long[] {2, 6, 8},
+				new long[] {6, 4, 6});
+		List<Range> slices;
+
+
+		slices = new ArrayList<>(initial);
+		Slices.addSlice(slices, Range.at(0, 1));
+		//        0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (0,1) [-]
+		// (2,6)     [-----------]
+		// (6,4)             [---------]
+		// (8,6)                 [-----------]
+		assertEquals(createSlices(
+				new long[] {0, 2, 6, 8},
+				new long[] {1, 6, 4, 6}), slices);
+
+
+		slices = new ArrayList<>(initial);
+		Slices.addSlice(slices, Range.at(0, 16));
+		//        0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (0,16)[-------------------------------]
+		assertEquals(createSlices(
+				new long[] {0},
+				new long[] {16}), slices);
+
+
+		slices = new ArrayList<>(initial);
+		Slices.addSlice(slices, Range.at(2, 8));
+		//        0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (2,8)     [-----------------]
+		// (8,6)                 [-----------]
+		assertEquals(createSlices(
+				new long[] {2, 8},
+				new long[] {8, 6}), slices);
+
+
+		slices = new ArrayList<>(initial);
+		Slices.addSlice(slices, Range.at(1, 10));
+		//        0 1 2 3 4 5 6 7 8 9 A B C D E F
+		// (1,10)  [---------------------]
+		// (8,6)                 [-----------]
+		assertEquals(createSlices(
+				new long[] {1, 8},
+				new long[] {10, 6}), slices);
+	}
+}
