35f110ac6a10f2f7c7754ced99916d8fbd7779c9
tpietzsch <tobias.pietzsch@gmail.com>
2026-03-04 12:38:46 +0100

    Add RootedKeyValueAccess interface

---
+++ src/main/java/org/janelia/saalfeldlab/n5/RootedKeyValueAccess.java
@@ -0,0 +1,10 @@
+package org.janelia.saalfeldlab.n5;
+
+
+/**
+ * TODO: This is intented to eventually replace KeyValueAccess.
+ *  The plan is to incrementally replace usages of KeyValueAccess, adding methods here as needed.
+ */
+public interface RootedKeyValueAccess {
+	
+}
