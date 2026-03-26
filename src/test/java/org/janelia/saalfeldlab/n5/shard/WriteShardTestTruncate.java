3d9ff4ba776866a1ee9186f7e151d49d6a9c9c4a
tpietzsch <tobias.pietzsch@gmail.com>
2026-03-26 15:08:34 +0100

    Make getDatasetAccess() protected and createDatasetAccess() private
    createDatasetAccess() can be private because we don't override it
    anywhere. (Maybe we did previously in n5-zarr?)
    
    getDatasetAccess() has to be protected for tests only.
    It is better for tests to use the canonical DatasetAccess instead of
    creating a separate one, as they did before using createDatasetAccess()

--- src/test/java/org/janelia/saalfeldlab/n5/shard/WriteShardTestTruncate.java
+++ src/test/java/org/janelia/saalfeldlab/n5/shard/WriteShardTestTruncate.java
@@ -73,7 +73,7 @@ public class WriteShardTestTruncate {
 				c1,
 				new RawCompression());
 
-		final DatasetAccess<int[]> datasetAccess = attributes.datasetAccess();
+		final DatasetAccess<int[]> datasetAccess = attributes.getDatasetAccess();
 		final PositionValueAccess store = new TestPositionValueAccess();
 
 		//	0       1       2       3       4       5       6
@@ -137,10 +137,9 @@ public class WriteShardTestTruncate {
 			super(dimensions, outerBlockSize, dataType, blockCodecInfo, dataCodecInfos);
 		}
 
-		public DatasetAccess datasetAccess() {
-
-			// to make this accessible for the test
-			return createDatasetAccess();
+		@Override // to make this accessible for the test
+		protected <T> DatasetAccess<T> getDatasetAccess() {
+			return super.getDatasetAccess();
 		}
 	}
 }
